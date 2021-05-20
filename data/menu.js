4695a78b9f1c7d96ec8fdea0b5a613bc37a28716
Emily Kim <60374637+itwassme@users.noreply.github.com>
2021-05-20 11:25:18 +0900

    Update menu.js

--- data/menu.js
+++ data/menu.js
@@ -3,26 +3,26 @@ const menu = [
     children: 'Lost in Toyko',
     logo: true,
     className: 'order-3-ns w-100 w-30-ns mb3 mb0-ns',
-    href: 'index.html'
+    href: '#'
   },
   {
     children: 'About',
     className: 'order-1-ns w-20',
-    href: 'about.html'
+    href: '#'
   },
   {
     children: 'Tickets',
     className: 'order-2-ns w-20',
-    href: 'tickets.html'
+    href: '#'
   },
   {
     children: 'Journal',
     className: 'order-4-ns w-20',
-    href: 'journal.html'
+    href: '#'
   },
   {
     children: 'Contact',
     className: 'order-5-ns w-20',
-    href: 'contact.html'
+    href: '#'
   }
 ];
